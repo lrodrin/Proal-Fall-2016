5cd19e31d60e8af54a7bcfa06443f8676f374ac6
Laura <laura.rodriguez.navas@upc.edu>
2016-10-29 18:11:37 +0200

    list 1

---
+++ inici(1).py
@@ -0,0 +1,26 @@
+def welcome(name):
+    print("Hello %s, you are welcome!" % name)
+
+
+def max_min(x, y):
+    return "(%d, %d)" % (max(x, y), min(x, y))
+
+
+def integer_division(a, b):
+    if a >= 0 and b >= 0 and b != 0:
+        return "(%d, %d)" % (a / b, a % b)
+
+
+def digit_count(n):
+    count = 1
+    while (int(n / 10)) is not 0:
+        count += 1
+        n = int(n / 10)
+    return count
+
+
+if __name__ == "__main__":
+    welcome("King Kong")
+    print(max_min(-3, 5))
+    print(integer_division(14, 3))
+    print(digit_count(6543))
