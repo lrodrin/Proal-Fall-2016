3de636c2ffcd66d84d57ae62239b651e19957f0b
laura <laura.rodriguez.navas@upc.edu>
2016-11-07 20:06:18 +0100

    no commit

--- List3/lower_&_upper.py
+++ List3/lower_&_upper.py
@@ -1,7 +1,8 @@
 def lowupp(info):
-    if info[0] == "_":
-        return info.lower().lstrip(info[0])
-    elif info[0] == "^":
-        return info.upper().lstrip(info[0])
-    else:
-        return info
+    if len(info) > 0:
+        if info.startswith("_"):
+            return info.lower().lstrip(info[0])
+        elif info.startswith("^"):
+            return info.upper().lstrip(info[0])
+        else:
+            return info
