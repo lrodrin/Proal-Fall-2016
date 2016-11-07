83c688b001fad8c97380a8b2c02a8b77f760dece
laura <laura.rodriguez.navas@upc.edu>
2016-11-07 17:30:49 +0100

    versión de lower&upper sin for's

--- List3/lower_&_upper.py
+++ List3/lower_&_upper.py
@@ -1,7 +1,7 @@
 def lowupp(info):
     if info[0] == "_":
-        print(info.lower().lstrip(info[0]))
+        return info.lower().lstrip(info[0])
     elif info[0] == "^":
-        print(info.upper().lstrip(info[0]))
+        return info.upper().lstrip(info[0])
     else:
-        print(info)
+        return info
