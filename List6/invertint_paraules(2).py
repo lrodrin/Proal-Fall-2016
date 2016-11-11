671f57c6aef1f04eee4c25480b49eeebc689da80
laura <laura.rodriguez.navas@upc.edu>
2016-11-11 02:22:36 +0100

    List6

--- List6/invertint_paraules(2).py
+++ List6/invertint_paraules(2).py
@@ -1,12 +1,12 @@
 from jutge import read
 
 n = read(int)
-k = 0
-while k < n:
+k = n
+while k > 0:
     res = []
     word = read(str)
     for i in range(len(word)):
         res.append(word[i])
     print(''.join(reversed(res)))
-    k += 1
+    k -= 1
 
