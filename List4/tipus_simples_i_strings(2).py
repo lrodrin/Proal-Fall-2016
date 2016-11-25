d71a0b77a777199d7ad860055f230c039bd30a95
Laura <laura.rodriguez.navas@upc.edu>
2016-11-25 21:08:50 +0100

    tipus simples i strings 2

--- List4/tipus_simples_i_strings(2).py
+++ List4/tipus_simples_i_strings(2).py
@@ -17,22 +17,29 @@ def word_count(s):
 def kth_word(s, k):
     if k >= 1:
         cont = 1
-        res = ""
         for word in s.split():
             if cont == k:
                 return word
-            cont +=1
+            cont += 1
 
-def suc_word(n):
-    pass
+
+def suc_word(s):
+    for word in s.split():
+        for c in word:
+            if c.isupper():
+                return word
 
 
 def drawA(n):
-    pass
+    if n >= 3:
+        for i in range(1, n):
+            if i == 3:
+                print("*" * n)
+            print("*" + ' ' * i + "*")
 
 
 print(um_count("Qui invenit amicum invenit thesauruM"))
 print(word_count("Alea iacta          est"))
 print(kth_word("Alea iacta est", 3))
-suc_word("qui invenit amiCum invenit thesauruM")
+print(suc_word("qui invenit amiCum invenit thesauruM"))
 drawA(5)
