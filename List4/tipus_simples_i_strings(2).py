0b97d6d7abc3e8dfcb9af19179cbc6f14537dd85
lrodrin <lrodrin@gmail.com>
2017-11-13 15:29:08 +0100

    print(um_count("Qui invenit amicum invenit thesauruM")) print(word_count("Alea iacta          est")) print(kth_word("Alea iacta est", 3)) print(suc_word("qui invenit amiCum invenit thesauruM"))

--- List4/tipus_simples_i_strings(2).py
+++ List4/tipus_simples_i_strings(2).py
@@ -1,41 +1,39 @@
 def um_count(s):
-    cont = 0
-    for word in s.split():
-        if "um" in word:
-            cont += 1
-    return cont
+    subs = "um"
+    return s.count(subs, 0, len(s))
 
 
 def word_count(s):
     cont = 0
-    for word in s.split():
-        if word in s:
-            cont += 1
+    for _ in s.split():
+        cont += 1
     return cont
 
 
 def kth_word(s, k):
     if k >= 1:
-        cont = 1
-        for word in s.split():
-            if cont == k:
-                return word
-            cont += 1
+        word = s.split()
+        return word[k - 1]
 
 
 def suc_word(s):
+    result = ""
     for word in s.split():
         for c in word:
             if c.isupper():
-                return word
+                result = word
+                break
+        else:
+            continue
+        break
+    return result
 
 
 def drawA(n):
     pass
 
-
-print(um_count("Qui iumnvenit amicum invenit thumesauruM"))
-print(word_count("Alea iacta          est            hehe"))
-print(kth_word("Alea iacta est", 2))
-print(suc_word("qui invenit amicum iNvenit thesauruM"))
+print(um_count("Qui invenit amicum invenit thesauruM"))
+print(word_count("Alea iacta          est"))
+print(kth_word("Alea iacta est", 3))
+print(suc_word("qui invenit amiCum invenit thesauruM"))
 drawA(5)
